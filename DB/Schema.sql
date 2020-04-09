38103c4f854dff0e0dd4e321e879e2e4f8872ecd
Jeretc79 <masonjeretc@gmail.com>
2020-04-08 18:01:17 -0700

    updated db file

--- DB/Schema.sql
+++ DB/Schema.sql
@@ -9,3 +9,10 @@ CREATE TABLE burgers
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
+
+CREATE TABLE history
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	PRIMARY KEY (id)
+);
